c7715104a9783d073bf76fb48d1f48bccb8ccb10
karlicoss <karlicoss@gmail.com>
2023-06-04 20:23:22 +0100

    general: adjust pytest so it runs tests inside namespace packages properly

--- src/karlicoss_pymplate/module.py
+++ src/karlicoss_pymplate/module.py
@@ -4,7 +4,9 @@ if typing.TYPE_CHECKING:
     # reveal_type(__file__)
     pass
 
-# NOTE: this doesn't work with tox -e py3, it thinks that the package name here is module.py and can't do a relative import
-# https://github.com/pytest-dev/pytest/issues/1927
-# https://github.com/pytest-dev/pytest/issues/2371
-# from .subpackage import test
+
+def test_module_name() -> None:
+    # just in case because pytest might have some shenanigans with test module names depending on args..
+    assert __name__ == 'karlicoss_pymplate.module'
+
+from .subpackage.submodule import test
--- src/karlicoss_pymplate/subpackage/submodule.py
+++ src/karlicoss_pymplate/subpackage/submodule.py
@@ -1,6 +1,10 @@
-def test():
+def test() -> None:
     # dummy test. it's also useful to have regardless so pytest is configured early on (it fails on no tests)
     assert True
+
+    # check names because pytest might mess this up (also see conftest.py for some hacks)
+    assert __package__ == 'karlicoss_pymplate.subpackage'
+    assert __name__    == 'karlicoss_pymplate.subpackage.submodule'
 
 
 ## just check that mypy works
