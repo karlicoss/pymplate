c0eff3baf9d59132124498cdd28dccca59d54120
Dima Gerasimov <karlicoss@gmail.com>
2025-05-12 23:52:45 +0100

    general: bring all files up to date with other projects

--- src/karlicoss_pymplate/module.py
+++ src/karlicoss_pymplate/module.py
@@ -1,4 +1,5 @@
 import typing
+
 if typing.TYPE_CHECKING:
     # sigh. apparently reveal_type is making mypy fail
     # reveal_type(__file__)
@@ -9,4 +10,10 @@ def test_module_name() -> None:
     # just in case because pytest might have some shenanigans with test module names depending on args..
     assert __name__ == 'karlicoss_pymplate.module'
 
-from .subpackage.submodule import test
+
+def check_doctest() -> None:
+    """
+    >>> 1 + 2
+    3
+    """
+    # this doctest should be collected by pytest and run
